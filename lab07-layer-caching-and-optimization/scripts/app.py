7d6b35d5eb292479ab32b15b363623811d44e41a
Abdul Rehman <abdul4rehman215@gmail.com>
2026-02-25 12:39:23 +0530

    Create app.py

---
+++ lab07-layer-caching-and-optimization/scripts/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from optimized container!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+# cache-test change
